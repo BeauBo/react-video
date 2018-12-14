cc6d2c3c595d78204aac8502b08258b84b77e574
Bo Huang <bo.huang@paytm.com>
2018-12-14 12:05:25 -0500

    Getting started

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const component = () => {
-	let element = document.createElement('div');
+import _ from 'lodash';
 
+const component = () => {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['hello', 'webpack']);
+
+  return element;
 };
+
+document.body.appendChild(component());
