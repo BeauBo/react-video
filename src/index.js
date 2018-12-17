d9d13526cfe9ce54700e5c6592b5bce3b49442a8
Bo Huang <bo.huang@paytm.com>
2018-12-17 10:07:55 -0500

    assest management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './Avatar.jpg';
+import Data from './data.xml';
 
 const component = () => {
   const element = document.createElement('div');
   element.innerHTML = _.join(['hello', 'webpack']);
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 };
